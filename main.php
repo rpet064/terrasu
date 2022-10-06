1fdb47ccf3ad54bb484567846fa967c968bb138c
rpet064 <93119631+rpet064@users.noreply.github.com>
2022-10-06 18:32:32 +1300

    counter component finished

--- main.php
+++ main.php
@@ -4,28 +4,40 @@ include('header.php');
 <div>
 
 <?=
- $current_question = include('api.php')[0];
+ $current_question = include('api.php');
 ?>
 
 <?php
-      if(
-        isset(
-          $_POST['button1'])) {
-          echo "You guessed True";
-      }
-      if(
-        isset(
-          $_POST['button2'])) {
-          echo "You guessed False";
-      }
+session_start();
+
+if (! isset($_SESSION["counter"]) ){
+  $_SESSION["counter"] = 0;
+  }
+
+  if(
+    isset($_POST['button1'])) {
+      ++$_SESSION['counter'];
+  }
+  if(
+    isset($_POST['button2'])) {
+      ++$_SESSION['counter'];
+   }
+
+   // reset counter
+  if(isset($_POST['reset'])) {
+   $_SESSION['counter'] = 0;
+}
   ?>
 
-  <form method="post">
+  <form method="POST">
   <input type="submit" name="button1"
                value="True"/>
          
  <input type="submit" name="button2"
                value="False"/>
+
+  <input type="submit" name="reset" value="Reset" />
+  <br/><?php echo $_SESSION['counter']; ?>
  </form>
 
 </div>
