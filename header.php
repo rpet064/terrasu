8464bca0feb082ccca048b86f861f68bd71904df
rpet064 <93119631+rpet064@users.noreply.github.com>
2022-10-09 12:04:58 +1300

    css body attribute relative disabled buttons - bug fixed

--- header.php
+++ header.php
@@ -1,7 +1,21 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" dir="ltr">
 <head>
-<title>How to put PHP in HTML</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <!-- Google  Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Pacifico&display=swap" rel="stylesheet">
+  <!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+  <link media="Screen" src="./css/styles.css" type="text/css" rel="stylesheet">
+  <!-- favicon.ico -->
+  <link rel="icon" type="image/png" sizes="32x32" href="./Images/favicon.ico">
 </head>
 <body>
 <div>Welcome to Terrasu, a PHP Geography Quiz Game</div>
--- main.php
+++ main.php
@@ -1,9 +1,13 @@
+
+
 <?php
 $header = include('header.php');
 ?>
 <div>
+  <!-- stylesheet -->
+<style><?php include('./css/styles.css'); ?></style>
 
-<script type="text/javascript">
+<script type="text/javascript"> 
 function checkfunction(obj){
   $.post("your_url.php",$(obj).serialize(),function(data){
    alert("success");
@@ -12,6 +16,8 @@ function checkfunction(obj){
    }
 
 </script>
+
+<button>Click me </button>
 
 <?php
 
@@ -55,27 +61,27 @@ if (! isset($_SESSION["counter"]) ){
 }
   ?>
   <br/><br/>
-  <?php echo 'Question ' . $_SESSION['counter']; ?>
-  <?php echo 'Your Score is ' . $_SESSION['score']; ?>
+  <h3><?php echo 'Question ' . $_SESSION['counter']; ?></h3>
+  <h3><?php echo 'Your Score is ' . $_SESSION['score']; ?></h3>
   <br/><br/>
 
   
   <form  onsubmit="return checkfunction(this)" method="post">
-  <input type="submit" name="True"
+  <input class='true-btn' type="submit" name="True"
                value="True" onclick="return checkfunction(this)"/>
          
- <input type="submit" name="False"
+ <input class='false-btn'type="submit" name="False"
                value="False" onclick="return checkfunction(this)"/>
 
-  <input type="submit" name="reset" value="Reset" onclick="return checkfunction(this)"/>
+  <input class='reset-btn'type="submit" name="reset" value="Reset" onclick="return checkfunction(this)"/>
  </form>
 
 </div>
 <!-- stop page reload after form submission -->
-<script>
+<!-- <script>
     if ( window.history.replaceState ) {
         window.history.replaceState( null, null, window.location.href );
     }
-</script>
+</script> -->
 </body>
 </html>
